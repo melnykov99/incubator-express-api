4c0c0d9a10b1fdbffd14b16a4efbd02604f7e1d0
Dmitry Melnykov <melnikov.d@wazzup.online>
2023-09-12 22:41:20 +0300

    add comments in index.d

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,12 @@
 import {UserInDB} from "./usersTypes";
 
 /**
- *
+ * создаем глобальную область видимости, которая охватывает все файлы в проекте
+ * определяем пространство имен Express в этой глобальной области видимости
+ * расширяем интерфейс Request в пространстве Express, добавляя к нему новое свойство user с типом UserInDB
  */
 declare global {
-    declare namespace Express {
+    namespace Express {
         export interface Request {
             user: UserInDB
         }
