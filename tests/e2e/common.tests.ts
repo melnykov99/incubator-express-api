c52c5f007f522fca108189d3c5e34942aa32943c
Dmitry Melnykov <melnikov.d@wazzup.online>
2023-09-16 00:34:35 +0300

    refactoring tests

--- tests/e2e/common.tests.ts
+++ tests/e2e/common.tests.ts
@@ -2,22 +2,34 @@ import request from 'supertest'
 import {app} from "../../src/setting"
 import {HTTP_STATUSES} from "../../src/utils/common/constants";
 
-describe('OPEN MAIN PAGE', () => {
-    it('should return status 200 and main page', async () => {
-        await request(app)
-            .get('/')
-            .expect(HTTP_STATUSES.OK_200, 'Main Page')
-    })
-})
+// Роут удаления всех данных. Очищает все коллекции в БД.
 describe('DELETE ALL DATA', () => {
     it('should delete all data and return status 204', async () => {
         await request(app)
             .delete('/testing/all-data')
             .expect(HTTP_STATUSES.NO_CONTENT_204)
     })
-    it('should return empty array after delete all data', async () => {
+    //если данных в коллекции нет, то в items будет пустой массив
+    it('should return empty items array after delete all data', async () => {
         await request(app)
             .get('/videos')
-            .expect(HTTP_STATUSES.OK_200, [])
+            .expect(HTTP_STATUSES.OK_200, {pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: []})
+    })
+    it('should return empty items array after delete all data', async () => {
+        await request(app)
+            .get('/blogs')
+            .expect(HTTP_STATUSES.OK_200, {pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: []})
+    })
+    it('should return empty items array after delete all data', async () => {
+        await request(app)
+            .get('/posts')
+            .expect(HTTP_STATUSES.OK_200, {pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: []})
+    })
+    //get users защищен basicAuth, поэтому передаем нужный Authorization
+    it('should return empty items array after delete all data', async () => {
+        await request(app)
+            .get('/users')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.OK_200, {pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: []})
     })
 })
